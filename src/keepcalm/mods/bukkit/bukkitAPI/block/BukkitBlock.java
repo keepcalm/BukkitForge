9d7d10ba1f6c6017372a078a1e145ab825cde948
keepcalm <keepcalm20687@gmail.com>
2013-01-13 11:10:51 +1100

    Fixed some illogical logic

--- src/keepcalm/mods/bukkit/bukkitAPI/block/BukkitBlock.java
+++ src/keepcalm/mods/bukkit/bukkitAPI/block/BukkitBlock.java
@@ -107,7 +107,7 @@ public class BukkitBlock implements Block {
 	}
 
 	public boolean setTypeIdAndData(final int type, final byte data, final boolean applyPhysics) {
-		if (net.minecraft.block.Block.blocksList[type] == null || type > 4096 || data > 16) {
+		if (net.minecraft.block.Block.blocksList[type] == null || type <= 4096 || data <= 16) {
 			return false;
 		}
 		if (applyPhysics) {
