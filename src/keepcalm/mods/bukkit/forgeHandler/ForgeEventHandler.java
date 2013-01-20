f1fbc8c5acf61383f4ed9965c2c8361dab80b5f7
SpoonsJTD <jtdspoons@hotmail.com>
2013-01-20 14:03:21 -0500

    Add ForgeSubscribe serverCmd -> PlayerCommandPreprocessEvent

--- src/keepcalm/mods/bukkit/forgeHandler/ForgeEventHandler.java
+++ src/keepcalm/mods/bukkit/forgeHandler/ForgeEventHandler.java
@@ -548,6 +548,8 @@ public class ForgeEventHandler {
 
 
 	}*/
+
+         @ForgeSubscribe
 	public void serverCmd(CommandEvent ev) {
 		if (ev.sender instanceof EntityPlayerMP) {
 			PlayerCommandPreprocessEvent bev=new PlayerCommandPreprocessEvent(new BukkitPlayer((EntityPlayerMP) ev.sender), "/" + ev.command.getCommandName() + " " + Joiner.on(' ').join(ev.parameters));
