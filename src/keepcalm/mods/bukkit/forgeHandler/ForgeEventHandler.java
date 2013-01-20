5938d0d265891c26de779559fa6cb634dabfe749
alexbegt <traincrazyb@aol.com>
2013-01-20 15:03:54 -0500

    Trying to fix commands

--- src/keepcalm/mods/bukkit/forgeHandler/ForgeEventHandler.java
+++ src/keepcalm/mods/bukkit/forgeHandler/ForgeEventHandler.java
@@ -548,8 +548,10 @@ public class ForgeEventHandler {
 
 
 	}*/
+	
+	//People be saying Commands dont work.
 
-        @ForgeSubscribe
+        //@ForgeSubscribe
 	public void serverCmd(CommandEvent ev) {
 		if (ev.sender instanceof EntityPlayerMP) {
 			PlayerCommandPreprocessEvent bev=new PlayerCommandPreprocessEvent(new BukkitPlayer((EntityPlayerMP) ev.sender), "/" + ev.command.getCommandName() + " " + Joiner.on(' ').join(ev.parameters));
