125006e1176dcc72b993b886d6d55480ae92fb87
Krim <git@wmchris.de>
2013-01-25 01:09:54 +0100

    Fixed Permission Bug, still added BukkitHumanEntity

---
+++ src/keepcalm/mods/bukkit/bukkitAPI/entity/BukkitHumanEntity.java
@@ -0,0 +1,14 @@
+package keepcalm.mods.bukkit.bukkitAPI.entity;
+
+import keepcalm.mods.bukkit.bukkitAPI.BukkitServer;
+import net.minecraft.entity.EntityLiving;
+
+public class BukkitHumanEntity extends BukkitEntityHuman
+{
+
+	public BukkitHumanEntity(BukkitServer server, EntityLiving entity) 
+	{
+		super(server, entity);
+	}
+
+}
