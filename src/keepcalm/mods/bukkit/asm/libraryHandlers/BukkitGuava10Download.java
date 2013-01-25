05c7eddce65384c51a276cf6a9cf8d979e431103
keepcalm <keepcalm20687@gmail.com>
2013-01-25 15:33:14 +1100

    Added SpecialSource dl to BukkitGuava10Download

--- src/keepcalm/mods/bukkit/asm/libraryHandlers/BukkitGuava10Download.java
+++ src/keepcalm/mods/bukkit/asm/libraryHandlers/BukkitGuava10Download.java
@@ -6,12 +6,14 @@ public class BukkitGuava10Download implements ILibrarySet {
 
 	@Override
 	public String[] getLibraries() {
-		return new String[] {"guava10-renamed.jar"};
+		return new String[] {"guava10-renamed.jar", "SpecialSource-1.3-SNAPSHOT.jar"};
 	}
 
 	@Override
 	public String[] getHashes() {
-		return new String[] {"be77eccdc699f3f8ec1623700ba0fa38270680fe"};
+		return new String[] {"be77eccdc699f3f8ec1623700ba0fa38270680fe", 
+				"4e74297853270b02eb00cdee6c92dc284a11e0e3"
+		};
 	}
 
 	@Override
