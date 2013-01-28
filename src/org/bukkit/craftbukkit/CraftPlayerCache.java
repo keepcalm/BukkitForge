4f9f2694b784d01f90d94d4ad2169083be56254d
keepcalm <keepcalm20687@gmail.com>
2013-01-28 14:15:35 +1100

    Reduced debug, but added an exception.

--- src/org/bukkit/craftbukkit/CraftPlayerCache.java
+++ src/org/bukkit/craftbukkit/CraftPlayerCache.java
@@ -14,20 +14,26 @@ public class CraftPlayerCache {
 	
 	public static CraftPlayer getCraftPlayer(EntityPlayerMP player) {
 		System.out.println("Looking for " + player.username + " in cache...");
+		try {
+			throw new Exception("This is some MOAR DEBUG :P");
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 		if (playerCache.containsKey(player.username.toLowerCase()))
 		{
-			System.out.println("Found them!");
+			//System.out.println("Found them!");
 			CraftPlayer ply = playerCache.get(player.username.toLowerCase());
 			if (ply.getHandle().isDead) {
 				System.out.println("Cached player's handle is dead, updating...");
 				// new player needed
 				//removePlayer(player.username);
 				ply.setHandle(player);
-				System.out.println("Returning " + ply);
+			//	System.out.println("Returning " + ply);
 				return ply;
 			}
 			else {
-				System.out.println("Returning " + ply);
+				//System.out.println("Returning " + ply);
 				return ply;
 			}
 		}
